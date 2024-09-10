d9403bc7ebc8245b4aab79ac44d2634c0bc9ab3b
Yujdestroyer11 <yujdestroyer@gmail.com>
2024-09-10 23:45:18 +0800

    Edited url of images

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,6 +1,10 @@
 import Headline1 from "./Headline1";
 import Headline2 from "./Headline2";
 import projectInfo from "../Info";
+import redtiger from "../../public/images/Redtiger.png"
+import vapeguru from "../../public/images/Vapeguru.png"
+import keeper from "../../public/images/Keeper.png"
+
 
 export default function Projects() {
   return (
@@ -8,21 +12,21 @@ export default function Projects() {
             <h2 class="text-center pt-3 display-6 fw-bold text-body-emphasis">Some of my work</h2>
             <div>
               <Headline1 
-              img={projectInfo[0].imgURL}
+              img={redtiger}
               name={projectInfo[0].projectName}
               description={projectInfo[0].description}
               href={projectInfo[0].href}
               tools={projectInfo[0].tools}
               />
               <Headline2 
-              img={projectInfo[1].imgURL}
+              img={vapeguru}
               name={projectInfo[1].projectName}
               description={projectInfo[1].description}
               href={projectInfo[1].href}
               tools={projectInfo[1].tools}
               />
               <Headline1 
-              img={projectInfo[2].imgURL}
+              img={keeper}
               name={projectInfo[2].projectName}
               description={projectInfo[2].description}
               href={projectInfo[2].href}
