49a1f81851caf8a759b7e6c868cf865d6550195a
Yujdestroyer11 <yujdestroyer@gmail.com>
2024-09-11 00:55:52 +0800

    Changed links for the photos

--- src/components/Headline1.jsx
+++ src/components/Headline1.jsx
@@ -10,7 +10,7 @@ export default function Headline1(props) {
                     <h3>Tools used:</h3>
                     <p class="lead rubik text-white">{props.tools}</p>
                     <h3>Link:</h3>
-                 <a href="https://red-tiger.vercel.app/"><p class="lead rubik text-white">{props.href}</p></a> 
+                 <a href={props.href}><p class="lead rubik text-white">{props.href}</p></a> 
                 </div>
                 <div class="col-md-5 order-md-1">
                   <a href={props.href}><img class="hover-work bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" src={props.img} alt="" srcset="" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"/></a> 
